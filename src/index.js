d7acdbf3b7baf4ef5c741621fececb221fbd74bf
swdecker <swdecker@gmail.com>
2019-11-24 18:55:54 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const handleClickFunction = (event)=>{
+  event.target.innerText = "Happy to be clicked"
+  // console.log(`callback function triggered by ${event.target}`)
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleClickFunction} />
   </div>,
   document.getElementById('root')
 );
